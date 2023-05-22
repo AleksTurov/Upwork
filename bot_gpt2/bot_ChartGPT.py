4e202a82396b063c06830cec2883f24c11387cb2
AleksTurov <Lexa-turov@yandex.com>
2023-05-22 22:14:43 +1000

    divided into folders v.1.0

---
+++ bot_gpt2/bot_ChartGPT.py
@@ -0,0 +1,100 @@
+import os
+import openai
+import telebot
+
+# Получаем токены из переменных окружения для безопасности github actions
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+# Задаем токен бота и ключ API
+bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
+openai.api_key = OPENAI_API_KEY
+
+# Задаем идентификатор группы
+group_id = -1001647255083  # replace with your group chat ID
+
+@bot.message_handler(func=lambda _: True)
+def handle_message(message):
+    # Используем 'try-except' для обработки ошибок при взаимодействии с OpenAI
+    try:
+        engine = "text-davinci-004"
+        prompt = message.text
+        completion = openai.Completion.create(engine=engine, prompt=prompt, temperature=1.5, max_tokens=4000)
+
+        # Проверяем наличие ожидаемого поля в ответе от OpenAI
+        if 'choices' in completion and len(completion.choices) > 1 and 'text' in completion.choices[0]:
+            bot.send_message(chat_id=group_id, text=completion.choices[1]['text'])
+        else:
+            bot.send_message(chat_id=group_id, text="Sorry, I couldn't understand your message.")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+bot.polling()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
