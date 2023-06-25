d43cc587db6952393e85d0a950c5394f8cca539b
AleksTurov <Lexa-turov@yandex.com>
2023-06-25 21:35:09 +1000

    add pdf doc

--- bot_gpt3.5/bot_gpt3.5.py
+++ bot_gpt3.5/bot_gpt3.5.py
@@ -137,5 +137,20 @@ def process_photo_message(message):
         bot.reply_to(message, "Sorry, an error occurred while processing your photo.")
 
 
+@bot.message_handler(content_types=['document'])
+def handle_document(message):
+    process_document_message(message)
+
+def process_document_message(message):
+    try:
+        file_info = bot.get_file(message.document.file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+
+        text = extract_text(io.BytesIO(downloaded_file))
+
+        bot.reply_to(message, text)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        bot.reply_to(message, "Sorry, an error occurred while processing your document.")
 
 bot.polling()
